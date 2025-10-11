af240094fed5facd149146beb166297635ad3f5d
Даниил Крылов <dfkylov@mai.education>
2025-10-11 11:17:17 +0300

    fixed wrong input error

--- src/main.py
+++ src/main.py
@@ -5,8 +5,10 @@ def main() -> None:
     accept_point = input("Вы любите котов? Введите число \"1\", если да, \"0\", если нет\n")
     if accept_point == '1':
         pass
-    else:
+    elif accept_point == '0':
         raise ConnectionRefusedError('Ошибка: Вы не имеете права пользоваться данным калькулятором')
+    else:
+        raise SyntaxError('Вы ввели что-то, не являющееся цифрой 0 или 1')
     expression = input('Отлично! \nВведите выражение в обратной польской нотации: ')
     answer = calculate_expression(expression)
     print(f'Ваш ответ: {answer}')
